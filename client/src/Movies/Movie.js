0e388f49aa6ee6140ce41b83efeccf260bc93e63
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-19 19:54:37 -0400

    working on updatemovie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import UpdateMovie from "./UpdateMovie"
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
